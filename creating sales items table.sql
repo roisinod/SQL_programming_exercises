464c97caeba644ece86bd5dc917e4692c5860fbd
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2021-01-26 10:43:59 +0000

    Add files via upload

---
+++ creating sales items table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE sales_item(
+item_id INT UNSIGNED NOT NULL,
+sales_order)=_id INT UNSIGNED NOT NULL,
+FOREIGN KEY (item_id)
+REFERENCES item(id),
+FOREIGN KEY (sales_order_id),
+REFERENCES sales_order(id),
+quantity INT NOT NULL,
+discount DECIMAL(3,2) NULL DEFAULT 0,
+taxable BOOL NOT NULL DEFAULT 0,
+sales_tax_rate DECIMAL(5,2) NOT NULL
+DEFAULT 0,
+id INT UNSIGNED NOT NULL
+AUTO_INCREMENT PRIMARY KEY
+);
