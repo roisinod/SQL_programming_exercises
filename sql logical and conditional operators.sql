3273d6b7cf7b751704db21d175a888d9b8ec32a1
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2021-01-29 16:41:32 +0000

    Add files via upload

--- sql logical and conditional operators.sql
+++ sql logical and conditional operators.sql
@@ -42,3 +42,23 @@ SELECT DISTINCT state
 FROM customer
 WHERE state IN ("CA", "NJ")
 ORDER BY state;
+#join tables
+SELECT item_id, price
+FROM item INNER JOIN sales_item
+ON item.id = sales_item.item_id
+ORDER BY item_id;
+
+SELECT item_id, price
+FROM item INNER JOIN sales_item
+ON item.id = sales_item.item_id
+AND price > 120.00
+ORDER BY item_id;
+#join 3 tables
+SELECT sales_order.id, sales_item.quantity, item.price,
+(sales_item.quantity * item.price) AS total
+FROM sales_order 
+JOIN sales_item
+ON sales_item.sales_order_id = sales_order.id
+JOIN item
+ON item.id = sales_item.item_id
+ORDER BY sales_order.id;
