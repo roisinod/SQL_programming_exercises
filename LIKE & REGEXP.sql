e8817446e81843557e07daa7f7bafa3549bb1e6c
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2021-02-03 11:49:50 +0000

    Add files via upload

--- LIKE & REGEXP.sql
+++ LIKE & REGEXP.sql
@@ -1,4 +1,14 @@
 SELECT first_name, last_name
 FROM customer
 WHERE first_name LIKE "M%";
+ #% gets characters that come after 'm'
+ 
+SELECT first_name, last_name
+FROM customer
+WHERE first_name LIKE 'D_____';
+#names that begin with D with a certain number of characters
+
+SELECT first_name, last_name
+FROM customer
+WHERE first_name LIKE 'D%' OR last_name LIKE '%n';
  
